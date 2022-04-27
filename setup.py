4e13eed7632eab41d0f17918fbd327a6c28e52f1
Artem Ustinov <austinov@vmware.com>
2022-04-27 01:16:22 -0700

    Explicitly Use Checkout Action.

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setuptools.setup(
         ],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
