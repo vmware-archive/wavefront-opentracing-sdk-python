4cf67eccfa89aba515018e2760ae5c5e754e8437
Artem Ustinov <austinov@vmware.com>
2022-04-27 01:32:58 -0700

    Bump Package Version to 2.3.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
 
 setuptools.setup(
     name='wavefront-opentracing-sdk-python',
-    version='2.3.2',
+    version='2.3.3',
     author='Tanzu Observability by Wavefront',
     author_email='chitimba@wavefront.com',
     url='https://github.com/wavefrontHQ/wavefront-opentracing-sdk-python',
